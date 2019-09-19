1c13a1f82acab2c9970eaa78620655264865aeb7
bingo618 <bin9850@163.com>
2019-09-19 19:26:19 +0800

    111

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,23 @@
 module.exports = {
-    title: '个人主页',
-    description: '胡斌的博客',
+    title: 'BINGO的博客',
+    description: '记录工作和生活',
     themeConfig: {
-        nav: [
-            { text: 'Home', link: '/' },
-            { text: 'javascript', link: '/javascript/' },
-            { text: 'Github', link: 'https://github.com/bingo618' }
+        nav: [{
+                text: '主页',
+                link: '/'
+            },
+            {
+                text: '前端开发',
+                link: '/javascript/'
+            },
+            {
+                text: '生活感悟',
+                link: '/life/'
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/bingo618'
+            }
         ],
         sidebar: {
             '/javascript/': [
--- docs/README.md
+++ docs/README.md
@@ -1,2 +1,15 @@
-# 主页
+---
+home: true
+heroImage: http://pvyhasjid.bkt.clouddn.com/blog/20190822/VHuiem2NGyju.jpg?imageslim
+actionText: 马上GET →
+actionLink: /javascript/
+features:
+- title: 前端开发
+  details: 记录前端开发中的笔记和文章。
+- title: 生活感悟
+  details: 记录生活的点滴。
+- title: Github
+  details: 个人的代码仓库。
+footer: MIT Licensed | Copyright © 2018-present Bingo
+---
 
